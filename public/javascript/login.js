ea77e8b52f2f39d3829641a291fbce1c38c59df9
Sean Gillespie <seene3@gmail.com>
2022-02-04 17:05:46 -0500

    finished? restaurant sign up

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -20,30 +20,6 @@ async function userLoginFormHandler(event) {
         alert(response.statusText);
       }
     }
-  }
-
-async function restaurantLoginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/restaurant/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/restaurant-dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-    }
 }
 
 async function userSignupFormHandler(event) {
@@ -75,23 +51,37 @@ async function userSignupFormHandler(event) {
 async function restaurantSignupFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#restaurant-signup').value.trim();
-    const email = document.querySelector('#restaurant-email-signup').value.trim();
-    const password = document.querySelector('#restaurant-password-signup').value.trim();
+    const business_name = document.querySelector('#business_name_signup').value.trim();
+    const business_email = document.querySelector('#business_email_signup').value.trim();
+    const business_password = document.querySelector('#business_password_signup').value.trim();
+    const business_occupancy = document.querySelector('#business_occupancy_signup').value.trim();
+    const business_address = document.querySelector('#business_address_signup').value.trim();
+    const business_phone = document.querySelector('#business_phone_signup').value.trim();
+    const business_hours_open = document.querySelector('#business_hours_open_signup').value.trim();
+    const business_hours_close = document.querySelector('#business_hours_close_signup').value.trim();
+    const business_website = document.querySelector('#business_website_signup').value.trim();
+    const business_image = document.querySelector('#business_image_signup').value.trim();
   
-    if (username && email && password) {
+    if (business_name && business_email && business_password && business_occupancy && business_address && business_phone && business_hours_open && business_hours_close && business_website && business_image ) {
       const response = await fetch('/api/restaurant', {
         method: 'post',
         body: JSON.stringify({
-          username,
-          email,
-          password
+          business_name,
+          business_email,
+          business_password,
+          business_occupancy,
+          business_address,
+          business_phone,
+          business_hours_open,
+          business_hours_close,
+          business_website,
+          business_image
         }),
         headers: { 'Content-Type': 'application/json' }
       });
   
       if (response.ok) {
-        document.location.replace('/restaurant-dashboard/');
+        document.location.replace('/dashboard/');
       } else {
         alert(response.statusText);
       }
@@ -99,6 +89,5 @@ async function restaurantSignupFormHandler(event) {
 }
   
 document.querySelector('.user-login-form').addEventListener('submit', userLoginFormHandler);
-document.querySelector('.restaurant-login-form').addEventListener('submit', restaurantLoginFormHandler);
 document.querySelector('.user-signup-form').addEventListener('submit', userSignupFormHandler);
 document.querySelector('.restaurant-signup-form').addEventListener('submit', restaurantSignupFormHandler);
