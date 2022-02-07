b4bb71ebb8810d3f609fc8f8339a2af4207175af
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-02-06 21:59:00 -0500

    search bar renders page on submit

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,15 +1,15 @@
-let data = [];
+let namesArr = [];
 
 const autocomplete = document.querySelector("#autocomplete");
 const resultsHTML = document.querySelector("#results");
 // const dataInput = document.querySelector(".search-results");
 
-function getNames () {
+function getNames() {
     const list = document.querySelector('.search-results').getElementsByTagName('li');
-   
+
     for (var i = 0; i < list.length; i++) {
         var arrValue = list[i].innerHTML;
-        data.push(arrValue);    
+        namesArr.push(arrValue);
     }
 }
 
@@ -29,9 +29,9 @@ autocomplete.oninput = function () {
 
 function getResults(input) {
     const results = [];
-    for (i = 0; i < data.length; i++) {
-        if (input === data[i].slice(0, input.length)) {
-            results.push(data[i]);
+    for (i = 0; i < namesArr.length; i++) {
+        if (input === namesArr[i].slice(0, input.length)) {
+            results.push(namesArr[i]);
         }
     }
     return results;
@@ -42,6 +42,36 @@ resultsHTML.onclick = function (event) {
     autocomplete.value = setValue;
     this.innerHTML = "";
 };
+
+async function searchSubmitHandler(event) {
+    event.preventDefault();
+    console.log('test')
+
+    const businessName = document.querySelector('input[id="autocomplete"]').value.trim();
+
+    const response = await fetch(`/api/restaurant`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data[0].business_name);
+                for (var i = 0; i < data.length; i++) {
+                    if(data[i].business_name === businessName) {
+                        const id = data[i].id;
+                        document.location.replace(`/restaurant/${id}`);
+                    }
+                }
+            });
+        } else {
+            alert("")
+        }
+    });
+};
+
+document.querySelector('.search-submit-form').addEventListener('submit', searchSubmitHandler);
 
 window.onload = getNames;
 
