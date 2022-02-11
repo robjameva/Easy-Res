ed933d58f96e0c94945949d0e68694de8740601b
Robert Evanik <rob.evanik@gmail.com>
2022-02-10 21:38:36 -0500

    testing reservation delete

---
+++ test.js
@@ -0,0 +1,14 @@
+const { Reservation, Restaurant } = require('./models');
+const sequelize = require('./config/connection');
+
+function wipeReservations() {
+    Reservation.destroy({
+        where: {},
+        truncate: true
+    })
+        .then(dbPostData => {
+            console.log(dbPostData);
+            console.log('wiped');
+        })
+}
+wipeReservations();
